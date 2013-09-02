4a1603607ea7149dd1c30334a3ac954cda19ac58
lixue <lixue100000@sina.com>
2013-09-02 11:32:43 +0800

    刷新

---
+++ src/com/lixueandroid/activity/TestScrollViewActivity.java
@@ -0,0 +1,57 @@
+package com.lixueandroid.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.lixue.lixueandroid.R;
+import com.lixueandroid.activity.view.PullToRefreshView;
+import com.lixueandroid.activity.view.PullToRefreshView.OnFooterRefreshListener;
+import com.lixueandroid.activity.view.PullToRefreshView.OnHeaderRefreshListener;
+
+
+public class TestScrollViewActivity extends Activity implements OnHeaderRefreshListener, OnFooterRefreshListener {
+	PullToRefreshView mPullToRefreshView;
+	TextView textview;
+	String text = "https://github.com/heavenxue/lixueAndroids/tree/766893edcb0fff26834a818bf3de060ea46efdc8#lixueandroids";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test_scrollview);
+		
+		mPullToRefreshView = (PullToRefreshView) findViewById(R.id.main_pull_refresh_view);
+		textview = (TextView)findViewById(R.id.textview);
+		textview.setText(text);
+		
+		mPullToRefreshView.setOnHeaderRefreshListener(this);
+		mPullToRefreshView.setOnFooterRefreshListener(this);
+
+	}
+
+	@Override
+	public void onFooterRefresh(PullToRefreshView view) {
+		mPullToRefreshView.postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				textview.setText(text+";"+text);
+				mPullToRefreshView.onFooterRefreshComplete();
+			}
+		}, 1000);
+	}
+
+	@Override
+	public void onHeaderRefresh(PullToRefreshView view) {
+		mPullToRefreshView.postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				// 设置更新时间
+				//mPullToRefreshView.onHeaderRefreshComplete("最近更新:01-23 12:01");
+				textview.setText(text+";"+text);
+				mPullToRefreshView.onHeaderRefreshComplete();
+			}
+		}, 1000);
+
+	}
+}
---
+++ src/com/lixueandroid/activity/adapter/DataAdapter.java
@@ -0,0 +1,41 @@
+package com.lixueandroid.activity.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.lixue.lixueandroid.R;
+
+public class DataAdapter extends BaseAdapter{
+		Context mContext;
+		List<Integer> listdata;
+		public DataAdapter(Context context,List<Integer> listdata){
+			mContext = context;
+			this.listdata=listdata;
+		}
+		@Override
+		public int getCount() {
+			return listdata.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return position;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if(convertView == null){
+				convertView = View.inflate(mContext, R.layout.grid_item, null);		
+			}
+			return convertView;
+		}
+	}
